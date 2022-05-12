6f04811b0be07b13efac0a39eadbc88df8f6dac7
Fernando Dornelles Rocha <fe.dornellesrocha@outlook.com>
2022-05-12 16:18:20 -0300

    exercises js block 4 day 1

---
+++ fundamentos/04.javascript/01.dia-primeirospassos/ex03/ex03.js
@@ -0,0 +1,11 @@
+const noteStudent = 10;
+
+if (noteStudent >= 80) {
+  console.log("Parabéns, você foi aprovado(a)!")
+}
+else if (noteStudent < 60) {
+  console.log('Você foi reprovado!')
+}
+else {
+  console.log('Você está na nossa lista de espera')
+}
---
+++ fundamentos/04.javascript/01.dia-primeirospassos/lessons/scripts.js
@@ -0,0 +1,28 @@
+// const myName = "Fernando";
+// const birthCity = "Taquari";
+// let birthYear = 2000;
+
+// console.log(myName, birthCity, birthYear)
+
+// birthYear = "2030"
+// birthCity = "Tk"
+
+// console.log(birthCity, birthYear)
+
+// a const é imutável, ela não pode ser reatribuida
+
+//  ---------------------------------------------------------
+
+let movie = "Avengers"; //string literal
+let score = 10.89; // number literal
+let isValid = true; //boolean
+let name; // undefined
+let color = null; //to-redefine
+
+// +, -, *, /, **
+
+let salary = 3500;
+
+console.log(salary + salary);
+
+// acrescentar ++, remover -- 

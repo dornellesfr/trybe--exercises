064e690776eb7b0432049cfd73a051aa5ac3deaa
Fernando Dornelles Rocha <fe.dornellesrocha@outlook.com>
2022-05-12 21:35:58 -0300

    5 Exercises completeds

---
+++ fundamentos/04.javascript/01.dia-primeirospassos/exercise02.js
@@ -0,0 +1,14 @@
+const n1 = 29;
+const n2 = 23;
+
+if (n1 > n2) {
+  console.log(`first number is bigger than second and that is ${n1}`);
+}
+
+else if (n2 > n1) {
+  console.log(`second number is bigger than first and that is ${n2}`);
+}
+
+else {
+  console.log(`They\'re(${n1} and ${n2}) the same number`);
+}

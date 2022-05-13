38b17137b565e25e85bad25e765afdc1eb66a4e3
Fernando <fe.dornellesrocha@outlook.com>
2022-05-13 19:11:15 -0300

    exercise 09 done

--- fundamentos/04.javascript/01.dia-primeirospassos/exercise08.js
+++ fundamentos/04.javascript/01.dia-primeirospassos/exercise08.js
@@ -8,7 +8,7 @@ if (num1 % 2 == 0 || num2 % 2 == 0 || num3 % 2 == 0) {
     ans = 'This sequence have a even number';
 }
 else {
-    ans = 'This sequence haven\'t a even number'
+    ans = 'This sequence haven\'t a even number';
 }
 
 console.log(ans)

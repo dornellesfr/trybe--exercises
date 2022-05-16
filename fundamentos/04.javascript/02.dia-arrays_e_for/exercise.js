5b138fcccc65048e45e37a2cfc5544d2d2de0055
Fernando Dornelles Rocha <fe.dornellesrocha@outlook.com>
2022-05-16 00:29:17 -0300

    Exercises 4.2 done

--- fundamentos/04.javascript/02.dia-arrays_e_for/exercise.js
+++ fundamentos/04.javascript/02.dia-arrays_e_for/exercise.js
@@ -5,7 +5,6 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
 
 // Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
-
 // A média aritmética é o resultado da soma de todos os elementos divido pelo número total de elementos.
 
 // Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
@@ -22,14 +21,94 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 
 
-
+// // EXERCÍCIO 1
 // for (let number of numbers) {
 //     console.log(number)
 // }
 
-let sum = 0;
-for(let pos = 0; pos < numbers.length; pos += 1) {
-    sum = sum + numbers[pos]
-}
+// // EXERCÍCIO 2
+// let sum = 0;
+// for(let pos = 0; pos < numbers.length; pos += 1) {
+//     sum = sum + numbers[pos];
+// }
 
-console.log(sum)
+// console.log(sum);
+
+// //EXERCÍCIO 3
+// let media = 0;
+// let sum = 0;
+// for(let pos = 0; pos < numbers.length; pos += 1) {
+//     sum = sum + numbers[pos];
+// }
+
+// console.log(sum / numbers.length)
+
+
+// // EXERCÍCIO 4
+// if ((sum / numbers.length) > 20) {
+//     console.log('Number bigger than twenty');
+// }
+// else {
+//     console.log('Number smallest than twenty');
+// }
+
+// // EXERCÍCIO 5
+// let biggest;
+// for(let pos = 0; pos < numbers.length; pos += 1) {
+//     if (numbers[pos] == numbers[0]) {
+//         biggest = numbers[pos];
+//     }
+//     else {
+//         if (numbers[pos] > biggest) {
+//             biggest = numbers[pos];
+//         }
+//     }
+// }
+// console.log(biggest);
+
+// // EXERCÍCIO 6
+// let odd = 0;
+// for (number of numbers) {
+// 	if (number % 2 != 0){
+// 		odd += 1
+// 	}
+// }
+// if (odd == 0) {
+// 	console.log('Theres no odds numbers')
+// }
+// else {
+// 	console.log(odd)
+// }
+
+// // EXERCÍCIO 7
+// let smallest;
+// for(let pos = 0; pos < numbers.length; pos += 1) {
+//     if (numbers[pos] == numbers[0]) {
+//         smallest = numbers[pos];
+//     }
+//     else {
+//         if (numbers[pos] < smallest) {
+//             smallest = numbers[pos];
+//         }
+//     }
+// }
+// console.log(smallest);
+
+
+// // EXERCÍCIO 8
+// let list = [];
+// for (let index = 1; index <= 25; index += 1) {
+// 	list.push(index);
+// }
+// console.log(list);
+
+
+
+// // EXERCÍCIO 9
+// let list = [];
+// let divided;
+// for (let index = 1; index <= 25; index += 1) {
+// 	divided = index / 2;
+// 	list.push(divided);
+// }
+// console.log(list);

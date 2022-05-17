a438a8231689e29f016d1aa5941218b647a96abb
Fernando <fe.dornellesrocha@outlook.com>
2022-05-17 18:17:11 -0300

    changes

---
+++ fundamentos/04.javascript/03.dia-algoritmo_e_logica_de_programacao/exercise04.js
@@ -0,0 +1,25 @@
+// let divisor = 2
+// let lista = [];
+
+// for (let i = 2; i <= 50; i += 1) {
+//   let primo = true;
+
+//   for (divisor = 2; divisor <= i; divisor += 1){
+//     if (i % divisor === 0) {
+//       primo = false;
+//       break;
+//     }
+//     else{
+//       if (i in lista) {
+//         break;
+//       }
+//       else {
+//         if (primo == true) {
+//           lista.push(i);
+//         }  
+//       }
+
+//     }
+//   }
+// }
+// console.log(lista);

5e35dbd228b8feb469b285f449892e1b89346e7f
Fernando Dornelles Rocha <fe.dornellesrocha@outlook.com>
2022-05-18 13:06:23 -0300

    exercise 05 done

--- fundamentos/04.javascript/04.dia-objetos-e-funcoes/5-function.js
+++ fundamentos/04.javascript/04.dia-objetos-e-funcoes/5-function.js
@@ -1,11 +1,13 @@
-array = [2, 3, 2, 5, 8, 2, 3];
+array = [2, 3, 3, 5, 8, 2, 3];
 let data = [];
 array = array.sort();
 let dataAll = [];
+biggest = 0;
+numBig = 0;
 
 // [2,2,2,3,3,5,8];
 
-function knowRepeated(arr) {
+function knowRepeated(array) {
   for (let c in array) {
     if (c == 0) {
       data.push(array[c]);
@@ -15,13 +17,26 @@ function knowRepeated(arr) {
         data.push(array[c]);
       }
       else{
-        dataAll.push(data);
-        data = [];
-        data.push(array[c]);
+          dataAll.push(data);
+          data = [];
+          data.push(array[c]);
       }
     }
   }
-  return dataAll
+  dataAll.push(data);
+  for (i in dataAll) {
+    if (i == 0) {
+      biggest = dataAll[i].length;
+      numBig = dataAll[i][0];
+    }
+    else {
+      if (dataAll[i].length > biggest) {
+        biggest = dataAll[i].length;
+        numBig = dataAll[i][0];
+      }
+    }
+  }
+  return numBig
 }
 
 console.log(knowRepeated(array));

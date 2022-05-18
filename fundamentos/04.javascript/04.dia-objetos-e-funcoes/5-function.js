934eae3060a0f7df455894fac0d29ba690f81e72
Fernando Dornelles Rocha <fe.dornellesrocha@outlook.com>
2022-05-18 12:11:25 -0300

    new ex 05

--- fundamentos/04.javascript/04.dia-objetos-e-funcoes/5-function.js
+++ fundamentos/04.javascript/04.dia-objetos-e-funcoes/5-function.js
@@ -3,24 +3,25 @@ let data = [];
 array = array.sort();
 let dataAll = [];
 
-
-[2,2,2,3,3,5,8];
+// [2,2,2,3,3,5,8];
 
 function knowRepeated(arr) {
   for (let c in array) {
-    if (array[c] == array[0]) {
-      data.push(array[c])
+    if (c == 0) {
+      data.push(array[c]);
     }
     else {
       if (array[c] == array[c - 1]) {
-        data.push(array[c])
+        data.push(array[c]);
       }
       else{
-        dataAll = data
+        dataAll.push(data);
         data = [];
+        data.push(array[c]);
       }
     }
   }
+  return dataAll
 }
 
 console.log(knowRepeated(array));

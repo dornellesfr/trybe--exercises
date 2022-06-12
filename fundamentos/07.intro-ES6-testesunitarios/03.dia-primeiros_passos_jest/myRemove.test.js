d2552a8f6a2bc66c53b490d1f3c8e067dde337a2
Fernando Dornelles <fe.dornellesrocha@outlook.com>
2022-06-12 18:21:28 -0300

    Exercises 2.0 done

--- fundamentos/07.intro-ES6-testesunitarios/03.dia-primeiros_passos_jest/myRemove.test.js
+++ fundamentos/07.intro-ES6-testesunitarios/03.dia-primeiros_passos_jest/myRemove.test.js
@@ -1,7 +1,14 @@
 const myRemove = require('./myRemove');
 
-test('testing myRemove array 1...4 removing 3 return the array expected', () => {
-  const arr = [1, 2, 3, 4, 5];
-  const num = 3;
-  expect(myRemove(arr, num)).toBe([1, 2, 4, 5]);
+describe('Tests using myRemove to return a array without the second parameter', () => {
+  test('returns of array [1, 2, 4, 5] = [1, 2, 4, 5]', () => {
+    const arr = [1, 2, 3, 4, 5];
+    expect(myRemove(arr, 3)).toEqual([1, 2, 4, 5]);
+  });
+  test('returns of array [1, 2, 3, 4] != [1, 2, 3, 4]', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+  });
+  test('returns of array [1, 2, 3, 4] = [1, 2, 3, 4]', () => {
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+  });
 });

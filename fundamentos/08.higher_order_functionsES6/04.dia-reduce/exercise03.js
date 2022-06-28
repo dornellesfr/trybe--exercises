f8ee70333a67a02d2981948bdcecb75037551c81
Fernando Dornelles <fe.dornellesrocha@outlook.com>
2022-06-28 11:13:56 -0300

    ex03 done

--- fundamentos/08.higher_order_functionsES6/04.dia-reduce/exercise03.js
+++ fundamentos/08.higher_order_functionsES6/04.dia-reduce/exercise03.js
@@ -64,5 +64,7 @@ const books = [
 const expectedResult = 43;
 
 function averageAge() {
-  
+  return books.reduce((acc, curr) => acc + (curr.releaseYear - curr.author.birthYear), 0)/books.length;
 }
+
+console.log(averageAge());
